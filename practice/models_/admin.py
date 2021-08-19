9706d4aa60c197e4d3740596f66e9a9dff193727
manujagobind <manujagobind@gmail.com>
2021-08-19 15:29:59 +0530

    Practice more models

--- practice/models_/admin.py
+++ practice/models_/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Person, Group, Membership
+from .models import Person, Group, Membership, Restaurant, Place
 
 
 admin.site.register(Person)
 admin.site.register(Group)
 admin.site.register(Membership)
+
+admin.site.register(Place)
+admin.site.register(Restaurant)
