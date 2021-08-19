6e617fd9351bb43092067dd9f638aa6bac8b5b0d
Gobind <manujagobind@users.noreply.github.com>
2021-08-19 15:30:23 +0530

    Practice more models (#2)

--- practice/models_/models.py
+++ practice/models_/models.py
@@ -20,3 +20,18 @@ class Membership(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     date_joined = models.DateField()
     invite_reason = models.CharField(max_length=64)
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "Place: %s" % self.name
+
+class Restaurant(Place):
+    serves_hot_dogs = models.BooleanField(default=False)
+    servex_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Restaurant: %s" % self.name
